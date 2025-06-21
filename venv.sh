cfe737948a84ed5ec16096927fe1305a9e33a0d6
0x004null <elmadisamir2002@gmail.com>
2025-06-21 20:39:54 +0000

    Create venv.sh
    it will be once we are able to use it ^^

---
+++ venv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash venv /ksh/csh
+# pre-version 
+# diss-asm
+
+#_venv var_ memory cache
+asm='$'
+
+echo $asm + ' ('asm')
+for i in 1 2 3 4
+  echo $i, $asm
+
+
+  unset
+
+
+
+
+  set $asm
+
+
+while [ ]
+
+
+
+end
