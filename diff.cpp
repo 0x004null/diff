56d560b28fe37031de5d6f9c48d523898f99364f
0x004null <elmadisamir2002@gmail.com>
2025-06-21 20:48:31 +0000

    Create diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+#define up_str
+#define down_str
+
+void _up_
+void _down_
+
+
+strict()
+
+int main(void){
+  if ()
+
+
+
+
+    if else 
+      void {
+            if ....
+          
+      }
+
+
+    else if
+
+
+
+
+
+      continue 
+
+  void strict_one{}
+  void strict_two{}
+  void strict_three{}
+
+  
+
+  case s_one()
+    break;
+  
+
+}
